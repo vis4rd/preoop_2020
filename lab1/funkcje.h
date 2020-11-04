b0bee903332985bf5175579bd5e1aaa990433876
Agent Olek <alagaesia.vrael@gmail.com>
2020-11-04 22:11:12 +0100

    Zmieniono komentarze na styl doxygena

--- lab1/funkcje.h
+++ lab1/funkcje.h
@@ -3,10 +3,42 @@
 #include <iostream>
 #include <iomanip>
 
+/* silnia - iteracyjna funkcja zwracajaca wartosc silni dla n
+	@param n - wartosc, z ktorej wyliczamy silnie
+	@return silnia z podanej wartosci
+*/
 int silnia(int);
+
+/* silnia - rekurencyjna funkcja zwracajaca wartosc silni dla n
+	@param n - wartosc, z ktorej wyliczamy silnie
+	@return silnia z podanej wartosci
+*/
 int silnia_rec(int);
+
+/* 	wypisywanie podanej tablicy liczb typu double
+	@param tablica liczb typu double
+	@param wielkosc wypisywanej tablicy
+*/
 void wypisz(double[], const int);
+
+/* 	wypisywanie podanej tablicy liczb typu double
+	@param tablica liczb typu double
+	@param czynnik przez ktory mnozymy wszystkie elementy tablicy
+	@param wielkosc wypisywanej tablicy
+*/
 void powiel(double[], double, const int);
+
+/* 	wypisywanie podanej tablicy liczb typu double
+	@param tablica liczb typu double
+	@param wielkosc wypisywanej tablicy
+	@return suma wartosci elementow podanej tablicy
+*/
 double suma(double[], const int);
+
+/* 	wypisywanie podanej tablicy liczb typu double
+	@param tablica liczb typu double
+	@param wielkosc wypisywanej tablicy
+	@return srednia z wartosci elementow podanej tablicy
+*/
 double srednia(double[], const int);
 
--- lab4/studList.h
+++ lab4/studList.h
@@ -3,12 +3,21 @@
 #include <iostream>
 #include <cstring>
 
+/*	Element struktury (listy)
+	@field wskaznik na nastepny element struktury
+	@field nazwa elementu
+*/
 typedef struct Node
 {
     struct Node *next;
     char *name;
 }Node;
 
+/*	Struktura przechowujaca liste struktur
+	@param nazwa listy
+	@param wskaznik na pierwszy element
+	@param wskaznik na ostatni element
+*/
 typedef struct StudList
 {
     char *name;
@@ -16,11 +25,41 @@ typedef struct StudList
     Node *tail;
 }StudList;
 
-void initList(StudList *, const char[]);  //inicjalizacja listy, nadanie nazwy strukturze
-void prepend(StudList *, const char[]);   //dodawanie elementu na poczatek listy
-bool isEmpty(const StudList *);     //sprawdzenie, czy lista jest pusta
-void print(const StudList *);       //wypisywanie zawartosci listy
-void removeLast(StudList *);        //usuniecie ostatniego elementu listy
-void clearList(StudList *);         //usuniecie calej zawartosci listy
-void removeList(StudList *);        //usuniecie struktury listy 
-                                    //...(wraz z nazwa i wszystkimi elementami)
+/*	Inicjalizacja listy
+	@param wskaznik na niezainicjalizowana strukure
+	@param nowa nazwa dla podanej struktury
+*/
+void initList(StudList *, const char[]);
+
+/*	Dodawanie elementu na poczatek listy
+	@param wskaznik do listy
+	@param nazwa nowego elementu
+*/
+void prepend(StudList *, const char[]);
+
+/*	Funkcja sprawdzajaca czy lista jest pusta
+	@param wskaznik do listy
+	@return true/false wskazujacy czy lista jest pusta
+*/
+bool isEmpty(const StudList *);
+
+/*	Wypisywanie calej listy
+	@param wskaznik do listy
+*/
+void print(const StudList *);
+
+/*	Usuwanie ostatniego elementu listy
+	@param wskaznik do listy
+*/
+void removeLast(StudList *);
+
+/*	Usuwanie wszystkich elementow listy
+	@param wskaznik do listy
+	@attention procedura ta korzysta z removeLast(StudList *)
+*/
+void clearList(StudList *);
+
+/*	Zwolnienie pamieci struktury przechowujacej liste
+	@param wskaznik do listy
+*/
+void removeList(StudList *);
